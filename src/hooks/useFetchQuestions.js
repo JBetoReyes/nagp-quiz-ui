734807431058ed99bcd3f1f0d424be89b7bf4f1e
JBetoReyes <josereyes55@hotmail.com>
2022-09-07 16:38:25 -0500

    adding husky

--- src/hooks/useFetchQuestions.js
+++ src/hooks/useFetchQuestions.js
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export const useFetchQuestions = () => {
-    const [questions, setQuestions] = useState([]);
-    useEffect(() => {
-        (async () => {
-            const res = await fetch('/quizzes');
-            const questionsResponse = await res.json();
-            setQuestions(questionsResponse);
-        })()
-    }, []);
-    return [questions];
-}
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('/quizzes');
+      const questionsResponse = await res.json();
+      setQuestions(questionsResponse);
+    })();
+  }, []);
+  return [questions];
+};
