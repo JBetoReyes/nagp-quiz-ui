7b1012053920e3ed87749dc8c95f58dc2a0471a9
Jose Alberto <josereyes55@hotmail.com>
2022-09-07 18:21:33 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const divElement = screen.getByText(/You scored/i);
+  const divElement = screen.getByText(/You scored making test to fail/i);
   expect(divElement).toBeInTheDocument();
 });
